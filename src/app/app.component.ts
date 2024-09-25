41d8ad66a5b88f1a66bfe96749efe064e1db8548
b.zamani@renolos.com <b.zamani@renolos.com>
2024-09-25 12:30:25 +0200

    create portfolio-header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { PortfolioHeaderComponent } from "./components/portfolio-header/portfolio-header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,PortfolioHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
