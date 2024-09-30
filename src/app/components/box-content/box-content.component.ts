1de3495b0525bb3727b99c4b24491a17119947de
b.zamani@renolos.com <b.zamani@renolos.com>
2024-09-30 13:47:45 +0200

    create about me box

---
+++ src/app/components/box-content/box-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-box-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './box-content.component.html',
+  styleUrl: './box-content.component.scss'
+})
+export class BoxContentComponent {
+
+}
