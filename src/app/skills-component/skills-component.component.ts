940205388f26c33af757f623c538b7419df7c610
b.zamani@renolos.com <b.zamani@renolos.com>
2024-10-01 13:20:37 +0200

    add skills components

--- src/app/skills-component/skills-component.component.ts
+++ src/app/skills-component/skills-component.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { BoxContentComponent } from '../components/box-content/box-content.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-skills-component',
   standalone: true,
-  imports: [BoxContentComponent],
+  imports: [BoxContentComponent,CommonModule],
   templateUrl: './skills-component.component.html',
   styleUrl: './skills-component.component.scss'
 })
-export class SkillsComponentComponent {
-
+export class SkillsComponentComponent { 
+  skills: { image: string; title: string }[] = [
+    { image: './assets/img/html5.png', title: 'HTML' },
+    { image: './assets/img/css.png', title: 'CSS' },
+    { image: './assets/img/javascript.png', title: 'JavaScript' },
+    { image: './assets/img/typescript.png', title: 'TypeScript' },
+    { image: './assets/img/angular.png', title: 'Angular' },
+    { image: './assets/img/firebase.png', title: 'Firebase' },
+    { image: './assets/img/html5.png', title: 'HTML' },
+    { image: './assets/img/css.png', title: 'CSS' },
+    { image: './assets/img/javascript.png', title: 'JavaScript' },
+    { image: './assets/img/typescript.png', title: 'TypeScript' },
+    { image: './assets/img/angular.png', title: 'Angular' },
+    { image: './assets/img/firebase.png', title: 'Firebase' },
+    { image: './assets/img/Growthmindset.png', title: 'Growth mindset' }
+  ];
 }
+
+
