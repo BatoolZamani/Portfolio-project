ff3af45b69dc10ff509f7370d89a3098d6e3a130
b.zamani@renolos.com <b.zamani@renolos.com>
2024-10-23 01:10:05 +0200

    responsive prpjects-component

--- src/app/projects-component/projects-component.component.ts
+++ src/app/projects-component/projects-component.component.ts
@@ -9,9 +9,9 @@ import { Component } from '@angular/core';
 })
 export class ProjectsComponentComponent {
   projects: { id:string;imageurl:string; title: string; skills: string[] }[] = [
-    { id: 'join', title: 'Join',imageurl:'assets/img/joinscreen.png',skills: ['Angular', 'TypeScript', 'HTML','Firebase'] },
-    { id: 'elPoloLoco', title: 'El Polo Loco',imageurl:'assets/img/elpololocoscreen.png', skills: ['HTML','CSS','JavaScript'] },
-    { id: 'daBubble', title: 'Da Bubble',imageurl:'assets/img/Dabubble.png', skills: ['Angular','Firebase','TypeScript'] }
+    { id: 'join', title: 'Join',imageurl:'assets/img/join-project.png',skills: ['JavaScript', 'Firebase', 'HTML','CSS'] },
+    { id: 'WizardManGame', title: 'Wizard Man Game',imageurl:'assets/img/wizard-man-project.png', skills: ['JavaScript','CSS','HTML'] },
+    { id: 'POKEMON', title: 'POKEMON',imageurl:'assets/img/pokedex-project.png', skills: ['JavaScript','ReST-API','HTML','CSS'] }
   ];
   
 }
